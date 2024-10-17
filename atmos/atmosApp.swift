e9e7e3b7fe74d7a956c16a168e3b04f6fb6c6810
Tom Joy <thwjoy@gmail.com>
2024-10-17 17:29:20 +0100

    Initial Commit

---
+++ atmos/atmosApp.swift
@@ -0,0 +1,17 @@
+//
+//  atmosApp.swift
+//  atmos
+//
+//  Created by Tom Joy on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct atmosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
